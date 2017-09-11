37ad3b0f70163f027d9e9183b6d75f24f30e68ae
Tianyun Zhang <tianyunz@med.umich.edu>
2017-09-11 17:57:14 -0400

    created react project; added home and login page; added routes

---
+++ bookwarm-react/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
+import {Route} from 'react-router';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/login" exact component={LoginPage} />
+        </div>
+    );
+  }
+}
+
+export default App;
